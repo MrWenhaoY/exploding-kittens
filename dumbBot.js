f57e76460d96642f58a054515a8b18975a135957
Wenhao <wenhaoyang2015@gmail.com>
2024-04-07 23:06:59 -0400

    Made dumbBot into a class

--- dumbBot.js
+++ dumbBot.js
@@ -1,11 +1,23 @@
-export function action(game, playerId, cards, deckSize) {
-    if (game.turn % 2 !== playerId) return; 
-    let handSize = Object.values(cards).reduce((acc, e) => acc + e, 0);
-    if (Math.random() < handSize / (handSize + deckSize - 1)) {
-        Object.keys(cards).forEach(card => {
-            if (Math.random() <= cards[card] / handSize) return game.play(playerId, card);
-            handSize -= cards[card];
-        })
+export class DumbBot {
+    constructor(game, playerId) {
+        this.game = game;
+        this.playerId = playerId;
+        game.turnHandlers.push(x => {
+            while(game.turn % 2 === this.playerId && game.winner === -1) {
+                this.action();
+            }
+        });
     }
-    return game.draw(playerId);
+    action() {
+        if (game.turn % 2 !== this.playerId) return; 
+        const cards = this.game.players[this.playerId];
+        let handSize = Object.values(cards).reduce((acc, e) => acc + e, 0);
+        if (Math.random() < handSize / (handSize + game.deck.length - 1)) {
+            Object.keys(cards).forEach(card => {
+                if (Math.random() <= cards[card] / handSize) return game.play(this.playerId, card);
+                handSize -= cards[card];
+            })
+        }
+        return game.draw(this.playerId);
+    }
 }
