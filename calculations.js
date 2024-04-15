5307dd5e6c551a3d161c7ec34332d8c3ebf7f64a
Wenhao <wenhaoyang2015@gmail.com>
2024-04-15 00:59:25 -0400

    Finish winChanceDP

--- calculations.js
+++ calculations.js
@@ -16,7 +16,7 @@ export function winChance(deckSize, skipCount) {
 export function winChanceDP(deckMax, skipMax) {
     skipMax += deckMax - 1;
     const result = [];     // [deck][skip]
-    for (let i = 0; i <= deckMax; i++) result.push(Array(skipMax));
+    for (let i = 0; i <= deckMax; i++) result.push(Array(skipMax + 1));
     
     for (let x = 1; x <= deckMax; x++) {
         for (let y = 0; y <= skipMax; y++) {
@@ -26,7 +26,8 @@ export function winChanceDP(deckMax, skipMax) {
             }
             const draw = x === 2 ? 0.5 : (x-1)/x * (1/(x-1) + (x-2)/(x-1)*result[x-2][y+1]);
             const skip = y <= 0 ? -1 : 1/x + (x-1)/x * result[x-1][y-1];
+            result[x][y] = Math.max(draw, skip);
         }
     }
-
+    return result;
 }
