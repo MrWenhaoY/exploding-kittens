fd8194a45e22da6e0cc04f435010d933d9c9069e
Wenhao <wenhaoyang2015@gmail.com>
2024-04-16 23:50:25 -0400

    Record experimental trials of skip noplay results

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import {Game} from "./game.js";
-import { DumbBot } from "./bots.js";
+import { DumbBot, SkipBot, NullBot } from "./bots.js";
 import * as c from "./calculations.js";
 import {winChanceDP} from "./Notes/1-skip.js";
 //import {fs} from "fs"; // Need to get fs though
@@ -10,22 +10,35 @@ a.forEach((arr, i) => {
     const x = {};
     arr.forEach((obj, j) => x[j] = obj);
     b[i] = x;
-});*/
+});
 
-console.log(JSON.stringify(b));
+console.log(JSON.stringify(b));*/
 
 window.calc = c;
 window.winChance = winChanceDP;
 
 const p0 = document.getElementById("p0-cards");
-//console.log('p0 is:', p0);
 const p1 = document.getElementById("p1-cards");
 const table = document.getElementById("deck")
 
-const game = new Game(p0, p1, table);
-const dumbBot = new DumbBot(game, 0, 500);
-const dumb2 = new DumbBot(game, 1, 500);//
-window.game = game;
-game.turnHandlers.forEach(x => x(game));
+window.records = [];
+
+
+
+function recordGame(delay=0) {
+    const game = new Game(p0, p1, table, false, false);
+    const dumbBot = new SkipBot(game, 0, -1);
+    const dumb2 = new NullBot(game, 1, -1);//
+    window.game = game;
+    game.gameEndHandlers.push(x => {
+        window.records.push(x.winner);
+        if (window.records.length > 99990) console.log(window.records);
+    })
+    game.turnHandlers.forEach(x => x(game));
+}
+
+// Doing 10000 runs of the game with just 9 skips and 1 kitten
+for (let i = 0; i < 100000; i++) recordGame(0);
+
 
 // Figure out a way to wait for player input
