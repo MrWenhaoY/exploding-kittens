4346c1c5cf4fcc961ffc5f8b26e89f86f0fe2aaf
Wenhao <wenhaoyang2015@gmail.com>
2024-04-16 23:48:55 -0400

    Refactor bots.js

--- bots.js
+++ bots.js
@@ -1,27 +1,8 @@
 class Bot {
-    constructor(game, playerId) {
+    constructor(game, playerId, sleepTime=200) {
         this.game = game;
         this.playerId = playerId;
-    }
-    action() {
-        if (this.game.turn % 2 !== this.playerId) return;
-    }
-}
 
-export class NullBot extends Bot {
-    constructor(game, playerId, sleepTime=200) {
-        super(game, playerId);
-        if (sleepTime < 0) {
-            game.turnHandlers.push(() => game.draw(this.playerId));
-        } else {
-            game.turnHandlers.push(() => setTimeout(() => game.draw(this.playerId), sleepTime));
-        }
-    }
-}
-
-export class DumbBot extends Bot{
-    constructor(game, playerId, sleepTime=200) {
-        super(game, playerId);
         if (sleepTime < 0) {
             game.turnHandlers.push(_ => {
                 while (game.turn % 2 === this.playerId && game.winner === -1) this.action();
@@ -43,15 +24,35 @@ export class DumbBot extends Bot{
         });
     }
     action() {
+        if (this.game.turn % 2 !== this.playerId) return;
+    }
+}
+
+export class NullBot extends Bot {
+    action() {
+        this.game.draw(this.playerId);
+    }
+}
+
+export class DumbBot extends Bot {
+    action() {
         if (this.game.turn % 2 !== this.playerId) return; 
         const cards = this.game.players[this.playerId];
         let handSize = Object.values(cards).reduce((acc, e) => acc + e, 0);
         if (Math.random() < handSize / (handSize + game.deck.length - 1)) {
             Object.keys(cards).forEach(card => {
-                if (Math.random() <= cards[card] / handSize) return game.play(this.playerId, card);
+                if (Math.random() <= cards[card] / handSize) return this.game.play(this.playerId, card);
                 handSize -= cards[card];
             })
         }
-        return game.draw(this.playerId);
+        return this.game.draw(this.playerId);
+    }
+}
+
+export class SkipBot extends Bot {
+    action() {
+        const cards = this.game.players[this.playerId];
+        if (cards["skip"]) return this.game.play(this.playerId, "skip");
+        else return this.game.draw(this.playerId);
     }
 }
