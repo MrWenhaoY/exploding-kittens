f83fa4080365a29cefbb159561ce8120cb8293d7
Wenhao <wenhaoyang2015@gmail.com>
2024-04-18 00:11:04 -0400

    Made runSim function in main.js

--- main.js
+++ main.js
@@ -21,24 +21,26 @@ const p0 = document.getElementById("p0-cards");
 const p1 = document.getElementById("p1-cards");
 const table = document.getElementById("deck")
 
-window.records = [];
 
 
-
-function recordGame(delay=0) {
-    const game = new Game(p0, p1, table, false, false);
-    const dumbBot = new SkipBot(game, 0, -1);
-    const dumb2 = new NullBot(game, 1, -1);//
-    window.game = game;
-    game.gameEndHandlers.push(x => {
-        window.records.push(x.winner);
-        if (window.records.length > 99990) console.log(window.records);
-    })
-    game.turnHandlers.forEach(x => x(game));
+function runSim(bot1, bot2, trials=1000) {
+    const records = [];
+    window.records = records;
+    for (let i = 0; i < trials; i++) {
+        const game = new Game(p0, p1, table, false, false);
+        const dumbBot = new bot1(game, 0, -1);
+        const dumb2 = new bot2(game, 1, -1);//
+        window.game = game;
+        game.gameEndHandlers.push(x => {
+            window.records.push(x.winner);
+            if (window.records.length === trials) console.log(window.records);
+        })
+        game.turnHandlers.forEach(x => x(game));
+    }
 }
 
 // Doing 10000 runs of the game with just 9 skips and 1 kitten
-for (let i = 0; i < 100000; i++) recordGame(0);
+//runSim(10000);
 
 
 // Figure out a way to wait for player input
