e7ce58a4efcc6b2b0ac2f2ce8a84810ead9e5ed1
Wenhao <wenhaoyang2015@gmail.com>
2024-05-04 15:27:17 -0400

    Create deepCopy utility function

--- utility.js
+++ utility.js
@@ -1,3 +1,14 @@
+// Does not work for circular references!!! (and functions)
+export function deepCopy(x) {
+    if (Array.isArray(x)) return x.map(deepCopy);
+    if (typeof(x) === "object") {
+        const copy = {};
+        Object.keys(x).forEach(key => copy[key] = deepCopy(x[key]));
+        return copy;
+    }
+    return x;
+}
+
 export function objAdd(obj, item, qty=1) {
     item in obj ? obj[item] += qty : obj[item] = qty;
 }
