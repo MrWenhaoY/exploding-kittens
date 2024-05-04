9fb1844d7ada3af613048be18ff61a4f61c5237a
Wenhao <wenhaoyang2015@gmail.com>
2024-05-03 23:49:06 -0400

    Create objSum utility function

--- utility.js
+++ utility.js
@@ -1,3 +1,7 @@
 export function objAdd(obj, item, qty=1) {
     item in obj ? obj[item] += qty : obj[item] = qty;
 }
+
+export function objSum(obj) {
+    return Object.keys(obj).reduce((acc, e) => acc + obj[e], 0);
+}
