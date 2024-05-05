6a81320bf50bd5f97d1e20d42a77ea3e9172c872
Wenhao <wenhaoyang2015@gmail.com>
2024-05-04 20:30:10 -0400

    Create objComp utility function

--- utility.js
+++ utility.js
@@ -13,6 +13,11 @@ export function objAdd(obj, item, qty=1) {
     item in obj ? obj[item] += qty : obj[item] = qty;
 }
 
+export function objComp(obj1, obj2) {
+    const k1 = Object.keys(obj1);
+    return k1.length === Object.keys(obj2).length && k1.every(k => obj1[k] === obj2[k]);
+}
+
 export function objSum(obj) {
     return Object.keys(obj).reduce((acc, e) => acc + obj[e], 0);
 }
